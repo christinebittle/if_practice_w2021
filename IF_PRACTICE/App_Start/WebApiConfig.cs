190578ccdf82223fb08b1ccdcc00b6580add10ce
Somebody <christinebitts@gmail.com>
2020-10-01 19:22:03 -0400

    Adjusted convention for webapiconfig.cs to include action attribute for all controllers

--- IF_PRACTICE/App_Start/WebApiConfig.cs
+++ IF_PRACTICE/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace IF_PRACTICE
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
